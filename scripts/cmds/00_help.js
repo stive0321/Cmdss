ebf1dd2f836eef11501a1b8d2d9504e853589045
stive0321 <165804330+stive0321@users.noreply.github.com>
2024-05-04 19:06:22 +0100

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "𝗔𝘀𝘁𝗿𝗮𝗹 𝗔𝗜";
+const doNotDelete = "Stanley stawa 😗🚬";
 /** 
 * @author NTKhang
 * @author: do not delete it
