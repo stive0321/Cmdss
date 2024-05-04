837692df41dea1d97b05168690db4f755ec94d7a
stive0321 <165804330+stive0321@users.noreply.github.com>
2024-05-04 19:03:16 +0100

    Update 00_0help.js

--- scripts/cmds/00_0help.js
+++ scripts/cmds/00_0help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "天 | 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄";
+const doNotDelete = "Stanley stawa 😗🚬";
 /** 
 * @author NTKhang
 * @author: do not delete it
@@ -61,7 +61,7 @@ module.exports = {
 												pageNotFound: "Trang %1 không tồn tại"
 								},
 								en: {
-												help: "🔴🟡🟢\n\n╔⏤⏤⏤╝❀╚⏤⏤⏤╗\n      %6\n\n%1\n\nPage [ %2/%3 ]\nCurrently, The Bot has %4 Commands \n\n 📜 » Type %5help <page> \n 📃 » Type %5help <command> \n\n \n",
+												help: "😗🚬🟡🟢\n\n╔⏤⏤⏤╝❀╚⏤⏤⏤╗\n      %6\n\n%1\n\nPage [ %2/%3 ]\nCurrently, The Bot has %4 Commands \n\n 📜 » Type %5help <page> \n 📃 » Type %5help <command> \n\n \n",
 												help2: "%1├───────⭔\n│ » Currently, the bot has %2 commands that can be used\n│ » Type %3help <command name> to view the details of how to use that command\n│ %4\n╰─────────────⭓",
 												commandNotFound: "My Lord, Command \"%1\" does not exist, My Apologies 😓",
 												getInfoCommand: "╭── NAME ────⭓\n│ %1\n├── INFO\n│ Description: %2\n│ Other names: %3\n│ Other names in your group: %4\n│ Version: %5\n│ Role: %6\n│ Time per command: %7s\n│ Author: %8\n├── Usage\n%9\n├── Notes\n│ The content inside <XXXXX> can be changed\n│ The content inside [a|b|c] is a or b or c\n╰──────⭔",
